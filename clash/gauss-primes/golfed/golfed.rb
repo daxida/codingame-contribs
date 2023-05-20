e9a6e86e32fa19a7b87c2f1d8c8e01dc6e8e7f52
daxida <80173028+daxida@users.noreply.github.com>
2023-05-20 13:12:34 +0200

    rb golf

--- clash/gauss-primes/golfed/golfed.rb
+++ clash/gauss-primes/golfed/golfed.rb
@@ -1,5 +1,26 @@
 # https://perldoc.perl.org/perlrun
 
+# 80
+
+#!ruby -anrprime
+x,y=$F.sort.map &:to_i
+p x+2<y%4?y.prime?: (x*x+y*y).prime?if y
+
+# 81
+
+#!ruby -anrprime
+x,y=$F.map(&:to_i).sort
+p x+2<y%4?y.prime?: (x*x+y*y).prime?if y
+
+# 94 @ricemath
+# Note that you can sort before casting to int since the first condition 
+# is unaffected and the second is only true if x == 0, which is always 
+# the first item even when string comparing (since 0 <= x, y < 100)
+
+require'prime'
+$<.max{x,y=_1.split.sort.map &:to_i
+p (x*x+y*y).prime?||x<1&&y%4>2&&y.prime?;0}
+
 # 96
 
 #!ruby -anrprime
