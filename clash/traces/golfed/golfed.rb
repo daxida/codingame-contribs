b9b11ac9df1c47f8f11c073d4278827decdff5bf
Rafa <galmaw@hotmail.com>
2023-10-20 21:56:07 +0200

    golf

--- clash/traces/golfed/golfed.rb
+++ clash/traces/golfed/golfed.rb
@@ -1,3 +1,10 @@
+# @Whale 82
+
+#!ruby -an
+*a,f=$F
+b,t=a.map{|x|x.split(/([#{f}])/).map{_1.chars.sort}}
+b&&p(b==t)
+
 #!ruby -an
 b,t,*f=$F
 p b.scan(r=/#{f}/)==t.scan(r)&&b.split(r).zip(t.split r).all?{_1&.chars&.sort==_2&.chars&.sort}if t
